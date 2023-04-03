5c000dde18a667390e16e7d0b0b389f53e76da0c
Kang Inyeong <52899340+KangInyeong@users.noreply.github.com>
2023-04-03 22:32:59 +0900

    Update 0403_linux_hi.c

--- 0403_linux_hi.c
+++ 0403_linux_hi.c
@@ -8,49 +8,49 @@
 
 int main(){
     int fd = open("./test.txt" , O_RDWR, 0);
-    
+    if(fd<0){
+          printf("FILE OPEN ERROR\n");
+          exit(1);
+    }
+
     char buf[1000] = {0,};
     int filesize = read(fd, buf, 1000-1);
     close(fd);
 
-    fd = open("./test.txt", O_RDWR, 0);
     int num;
     scanf("input number: %d", &num);
     
-    int flag = 0;
-    char newtxt[1000] = {0,};
-    while(flag < filesize){
-        char sentence[20] = {0,};
-        char tmp[2] = {0,};
-        int i = 0;
-        int flag = 0;
-        while(strcmp(tmp, "\n") != 0){
-            read(fd, tmp, 1);
-            //if(atoi(tmp) >= 1 && atoi(tmp) <= 100){
-            //    flag = 0;
-            //    if(atoi(tmp) == num){
-            //        flag = 1;
-            //        printf("hello");
-            //    }
-            //}
-            //if(flag == 0){
-            if(atoi(tmp) == num){
-                printf("4dk\n");
-            }
-                sentence[i] = tmp[0];
-                i++;
-            //}
-
-        }
-        //read(fd, tmp, 1);
-        //if(strcmp(tmp, "\n") == 0){
-        //    printf("ehlej\n");
-        //}
-        
-        printf("%s\n", sentence);
-        flag += i;
-
+    fd = open("./test.txt", O_RDWR | O_TRUNC, 0);
+    if(fd<0){
+          printf("FILE OPEN ERROR\n");
+          exit(1);
     }
+
+    int cnt = 1;
+    char newstr[1000] = {0,};
+    char tmp[10] = {0,};
+    int i = 0;
+    int idx = lseek(fd, i, SEEK_CUR);
+    read(fd, tmp, 9);
+    printf("%d %s\n", idx; tmp);
+    strcat(newstr, tmp);
+    i += 9;
+
+    while(idx < filesize){
+        if(cnt == num){
+            i += 9;
+            cnt++;
+        }
+        memset(tmp, 0, sizeof(tmp));
+        idx = lseek(fd, i, SEEK_CUR);
+        read(fd, tmp, 9);
+        printf("%d %s\n", idx, tmp);    
+        strcat(newstr, tmp);
+        i += 9;
+        cnt++;
+    }
+
+    printf("newstr : %s", newstr);
     close(fd);
 
     return 0;

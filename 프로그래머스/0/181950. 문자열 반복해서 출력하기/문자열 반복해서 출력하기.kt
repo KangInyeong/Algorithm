4b74a0927bb369177023971b06b1a3f496621ec4
Kang Inyeong <52899340+KangInyeong@users.noreply.github.com>
2024-04-28 00:25:14 +0900

    [level 0] Title: 문자열 반복해서 출력하기, Time: 188.65 ms, Memory: 47.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+	val input = readLine()!!.split(' ')
+    
+    val s1 = input[0].toString()
+    val a = input[1]!!.toInt()
+    
+    var answer = ""
+
+    for(i in 1..a)
+        answer += s1
+
+    println(answer)
+}
